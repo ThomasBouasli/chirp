56268f0ad8d27dd7ffd3585b9667e1932e747e8f
Thomas <thomas.bouasli@gmail.com>
2023-11-01 13:33:35 -0300

    removed emoji filter

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -90,11 +90,7 @@ export const postRouter = createTRPCRouter({
         .string()
         .emoji("Posts can only contain emojis 🤷‍♂️")
         .min(1, "Don't be shy 😳! Say something 💬! ")
-        .max(255, "Calm down there, Buckaroo 🤠. That's a bit too much 😅")
-        .regex(
-          /^(?!.*[🍆🍑👌💦]).*$/,
-          "Please don't! You`ve used an emoji that is in our no-no list.",
-        ),
+        .max(255, "Calm down there, Buckaroo 🤠. That's a bit too much 😅"),
     )
     .mutation(async ({ ctx, input }) => {
       const { success } = await ratelimit.limit(ctx.userId);
