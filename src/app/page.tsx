38de3abe86f8d60828b6e1b91d8121385d46fb11
Thomas <thomas.bouasli@gmail.com>
2023-11-02 00:39:00 -0300

    Added Logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { CreatePost } from "./_components/create-post";
 import { useMediaQuery } from "react-responsive";
 import { dark } from "@clerk/themes";
 import Feed from "./_components/feed";
+import Image from "next/image";
 
 export default function Home() {
   const { isLoaded } = useUser();
@@ -19,7 +20,10 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center">
       <div className="flex h-full w-full grow flex-col border-x border-zinc-700 md:max-w-2xl">
         <header className="flex h-20 items-center justify-between border-b border-zinc-700 p-4">
-          <span className="text-2xl font-bold">Chirp</span>
+          <div className="flex items-center">
+            <Image src="/logo.svg" alt="Chirp Logo" width={64} height={64} />
+            <span className="text-2xl font-bold">Chirp</span>
+          </div>
           <UserButton
             appearance={{ baseTheme: dark }}
             showName={!isMobile}
