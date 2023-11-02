0182c67fa99bddd7706a9ed256420097c2b4a564
Thomas <thomas.bouasli@gmail.com>
2023-11-02 01:01:20 -0300

    Added new logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,9 +20,9 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center">
       <div className="flex h-full w-full grow flex-col border-x border-zinc-700 md:max-w-2xl">
         <header className="flex h-20 items-center justify-between border-b border-zinc-700 p-4">
-          <div className="flex items-center">
-            <Image src="/logo.svg" alt="Chirp Logo" width={64} height={64} />
-            <span className="text-2xl font-bold">Chirp</span>
+          <div className="flex items-center gap-4">
+            <Image src="/logo.svg" alt="Chirp Logo" width={70} height={70} />
+            <h1 className="text-3xl font-bold">Chirp</h1>
           </div>
           <UserButton
             appearance={{ baseTheme: dark }}
